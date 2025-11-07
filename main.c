a70e08f9a0d8e8cb26d141ed6e228afc73a81d19
MathieuFaure <mathieu.faure@efrei.net>
2025-11-07 16:32:01 +0100

    test for part 1 in main.c

--- main.c
+++ main.c
@@ -6,9 +6,19 @@ int main() {
     // PART 1 :
 
     // test for step 1 :
-    t_adjacencyList *graph = readGraph("data/exemple_valid_step3.txt");
-    displayAdjacencyList(graph);
-    textFile(graph);
+
+    t_adjacencyList *graph1 = readGraph("filename.txt");
+    displayAdjacencyList(graph1);
+
+    // test for step 2 :
+
+    t_adjacencyList *graph2 = readGraph("filename.txt");
+    markovGraphOrNot(graph2);
+
+    // test for step 3 :
+
+    t_adjacencyList *graph3 = readGraph("filename.txt");
+    textFile(graph3);
 
     return 0;
 }
